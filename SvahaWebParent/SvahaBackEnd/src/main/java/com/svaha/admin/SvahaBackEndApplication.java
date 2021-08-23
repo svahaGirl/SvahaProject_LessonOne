24419ebe73565e253d8c9420a42173929fdada7f
svahaGirl <fyanton@hotmail.com>
2021-08-22 18:00:36 -0700

    Initial commit

---
+++ SvahaWebParent/SvahaBackEnd/src/main/java/com/svaha/admin/SvahaBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.svaha.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvahaBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvahaBackEndApplication.class, args);
+	}
+
+}
